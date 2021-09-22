d9bde5a5777b8035e1f3be96f21b56b33028c10c
Chris Pittman <ctp8441@g.rit.edu>
2021-09-22 15:32:32 -0400

    Added portal

---
+++ src/lib/components/Portal.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let portalChildren: HTMLDivElement;
+
+	onMount(() => {
+		// Already in app.html, guaranteed to exist
+		const portal = document.getElementById('portal');
+
+		// Don't care about wrapper elements, everything in portals is absolute anyway
+		portal.appendChild(portalChildren);
+	});
+</script>
+
+<div bind:this={portalChildren}>
+	<slot />
+</div>
