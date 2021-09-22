15c7317a237a025d6ae0bf3abfc829453b833f40
Chris Pittman <ctp8441@g.rit.edu>
2021-09-22 18:19:42 -0400

    Moved footer and navbar to layout folder

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,4 @@
+<footer class="p-5 bg-black flex flex-wrap justify-center gap-x-20 gap-y-5">
+	<a href="/about">About</a>
+	<div>{'</> by'} <a href="https://github.com/ChrisPittman343">Chris Pittman</a></div>
+</footer>
---
+++ src/lib/components/layout/Navbar.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import logo from '/logo.svg';
+	import logo_down from '/logo_down.svg';
+
+	let src = logo;
+	const onMouseEnter = (e) => (src = logo_down);
+	const onMouseLeave = (e) => (src = logo);
+</script>
+
+<nav class="p-2 mb-5 bg-black flex justify-center w-full">
+	<a href="/">
+		<img
+			{src}
+			alt="PXLS"
+			class="w-20 h-auto"
+			on:mouseenter={onMouseEnter}
+			on:mouseleave={onMouseLeave}
+		/>
+	</a>
+</nav>
