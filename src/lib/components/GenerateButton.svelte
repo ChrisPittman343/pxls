eee93daadb10dc159c286dc45c9c7f8b65998ea4
Chris Pittman <ctp8441@g.rit.edu>
2021-09-22 16:24:57 -0400

    Added "Generate" button + unused images for it

---
+++ src/lib/components/GenerateButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<button
+	class="px-4 py-2 h-min-content font-mono font-bold transition-colors bg-blue-500 hover:bg-blue-600 rounded-md"
+	on:click
+>
+	Generate
+</button>
