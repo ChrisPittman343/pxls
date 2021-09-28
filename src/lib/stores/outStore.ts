86d5096b8510cf978c834656e3672ca4aabf9b76
Chris Pittman <ctp8441@g.rit.edu>
2021-09-28 12:05:28 -0400

    Removed dumb comment

--- src/lib/stores/outStore.ts
+++ src/lib/stores/outStore.ts
@@ -2,9 +2,4 @@ import type { OutputFormat } from '$lib/types';
 import { writable } from 'svelte/store';
 
 export const outputFormat = writable<OutputFormat>('CSS');
-
-/**
- * `"{i}"` = iterator value (starting at 0),
- * `"{c}"` = color (current color format)
- */
 export const customRule = writable<string>('{i}. {c} = rgb({0}, {1}, {2}) if RGB');
